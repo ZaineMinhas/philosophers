43f3966cc6f8d7c3839f56fb411d2f5a008da1af
ZaineMinhas <zaineminhas5@gmail.com>
2021-11-23 18:36:22 +0100

    petit filtre pour le parsing

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:50:58 by zminhas           #+#    #+#             */
-/*   Updated: 2021/11/23 17:44:59 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/11/23 18:35:08 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,10 +58,16 @@ int	read_arg(t_table *table, int ac, char **av)
 	table->ti_eat = ft_atoi(av[3]);
 	table->ti_slp = ft_atoi(av[4]);
 	if (ac == 6)
+	{
 		table->nb_eat = ft_atoi(av[5]);
+		if (!table->nb_eat)
+			return (1);
+	}
 	else
 		table->nb_eat = 0;
 	table->is_dead = 0;
+	if (!table->nb_phil || table->nb_phil > 200 || table->ti_die < 60 || \
+		table->ti_eat < 60 || table->ti_slp < 60 || table->nb_eat < 0)
 	table->philo = (t_philo *)malloc(sizeof(t_philo) * table->nb_phil);
 	if (!table->philo)
 		return (1);
